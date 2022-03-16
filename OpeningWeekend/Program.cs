402c6f8b908597179d03438929dac37da5e0f7c2
varadib02 <barkabas02@gmail.com>
2022-03-16 12:48:57 +0100

    Add project files.

---
+++ OpeningWeekend/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OpeningWeekend
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
